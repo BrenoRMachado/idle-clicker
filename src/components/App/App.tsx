bcd52914940ff57e7de01cbaacba713f29957e7c
Breno Machado <burracha.6464@gmail.com>
2024-11-28 20:07:42 -0300

    adiciona app e header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
+import styles from "./App.module.css"
 export default function App() {
   return (
-    <article>
+    <article className= {styles.container}>
       <p>
         Vamos construir um jogo de clicker simples para entendermos os conceitos
         básicos de React.
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+      <header>
+        <h1>Idle Clicker</h1>
+      </header>
+    );
+  }
